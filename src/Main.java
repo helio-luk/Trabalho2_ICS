bd65aaa85732c6b4eb15249ca0d375d6a608c466
helio-luk <helio.ssjr@gmail.com>
2016-05-27 15:54:43 -0300

    tes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
-
+import sintese.*;
 public class Main {
+	
+	Oscilador o1;
+	
+	public Main(){
+		o1 = new Oscilador(10, 180, 90);
+		
+		Som som = new Som(o1, 0.5f, "simples.wav");
+		som.salvawave();
+		som.tocawave();
+	}
 	
 	public static void main (String args[]){
 		
-		System.out.println("teste");
+		new Main();
 		
 
 	}
